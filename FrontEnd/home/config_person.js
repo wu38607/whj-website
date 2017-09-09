14f524e72fdd593d3ee42cfd0177c05a336e7ffb
whj <wu38607@live.com>
2017-09-09 21:07:31 +0800

    update email

--- FrontEnd/home/config_person.js
+++ FrontEnd/home/config_person.js
@@ -1,7 +1,7 @@
 const personConfig = {
   title: 'Whj\'s Website',
   name: '吴泓江',
-  email: 'wu38607@live.com',
+  email: 'wu38607@gmail.com',
   git: 'https://github.com/wu38607',
   head: '/static/img/head.jpg'
 }
